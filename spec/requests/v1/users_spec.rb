bf011116af8728dc4e125ae616987d3a7c1e8fb4
Daniel Sperry <danny.sperry@gmail.com>
2017-04-09 18:56:18 -0700

    Adding reassurance checks on users for unauthenticated requests

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -1,34 +1,51 @@
 # spec/requests/v1/users_spec.rb
 describe "Users API" do
-  it 'sends a list of users' do
-    FactoryGirl.create_list(:user, 3)
+  context "/v1/users#index" do
+    it 'returns 401 if not authorized' do
+      # make sure it throws a 401 if unauthenticated
+      get '/v1/users'
+      expect(response.code.to_i).to eq 401
+    end
 
-    sign_in do |headers|
-      get '/v1/users', params: nil, headers: headers
-      # test for the 200 status-code
-      expect(response).to be_success
+    it 'sends a list of users' do
+      FactoryGirl.create_list(:user, 3)
 
-      # check to make sure the right amount of users are returned
-      # We created 3 at the beginning of the test and another with the #sign_in method, so 4 total
-      expect(json['data'].length).to eq(4)
+      sign_in do |headers|
+        get '/v1/users', params: nil, headers: headers
+        # test for the 200 status-code
+        expect(response).to be_success
+
+        # check to make sure the right amount of users are returned
+        # We created 3 at the beginning of the test and another with the #sign_in method, so 4 total
+        expect(json['data'].length).to eq(4)
+      end
     end
   end
 
-  it 'retrieves a specific user' do
-    user = FactoryGirl.create(:user)
+  context "/v1/users#show" do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
 
-    sign_in do |headers|
-      get "/v1/users/#{user.id}", params: { id: user.id }, headers: headers
+    it "returns 401 if not authorized" do
+      get "/v1/users/#{@user.id}"
+      expect(response.code.to_i).to eq 401
+    end
 
-      # test for the 200 status-code
-      expect(response).to be_success
+    it 'retrieves a specific user' do
+      sign_in do |headers|
+        get "/v1/users/#{@user.id}", headers: headers
 
-      # check that the user attributes are the same.
-      json_name_attr = json['data']['attributes']['name']
-      expect(json_name_attr).to eq(user.name)
+        # test for the 200 status-code
+        expect(response).to be_success
 
-      # ensure that private attributes aren't serialized
-      expect(json['private_attr']).to eq(nil)
+        # check that the user attributes are the same.
+        json_name_attr = json['data']['attributes']['name']
+        expect(json_name_attr).to eq(@user.name)
+
+        # ensure that private attributes aren't serialized
+        expect(json['private_attr']).to eq(nil)
+      end
     end
   end
 end
